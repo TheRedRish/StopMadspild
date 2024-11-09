5ad9da17824d3961456349be5ad5ee2ac421d3ae
Rune Hansen <rune@roeddik.net>
2024-11-09 12:16:41 +0100

    Added partner icons with link to ID

--- src/main/java/com/example/stopmadspild/controller/PrivateUserController.java
+++ src/main/java/com/example/stopmadspild/controller/PrivateUserController.java
@@ -16,12 +16,17 @@ public class PrivateUserController {
         // TODO Replace with fetch of partners
         ArrayList<Partner> partners = new ArrayList<>();
         Partner partner = new Partner();
-        partner.setName("Dit navn");
+        partner.setName("Salling Group");
         partner.setEmail("dit_email@eksempel.dk");
         partner.setAddress("Dit adresse");
         partner.setOpeningHours("Dine  opening hours");
-        partner.setWhoIs("Din beskrivelse");
+        partner.setWhoIs("Salling Group sælger dagligvarer og har en række forskellige butikker og kæder.\n" +
+                "\n" +
+                "Historien strækker sig mere end 100 år tilbage til 1906. Virksomheden har gennem årene udviklet sig markant, og det, der begyndte som en lille manufakturhandel i Aarhus, er i dag en international detailhandelskoncern. Butikker er kerneforretningen med flere end 1400 butikker i tre lande.\n" +
+                "\n" +
+                "Hovedkontoret ligger i Brabrand og ledes af administrerende direktør Per Bank. I Danmark har koncernen over 43.000 medarbejdere.");
         partner.setSinceWhen("Din start dato");
+        partner.setIconPictureLink("https://onethird.dk/wp-content/uploads/2022/02/Salling.png");
         partners.add(partner);
         partners.add(partner);
         partners.add(partner);
@@ -31,7 +36,7 @@ public class PrivateUserController {
         partners.add(partner);
         partners.add(partner);
         model.addAttribute("partners", partners);
-        return "private";
+        return "partners";
     }
 
     @GetMapping("/partner/{id}")
