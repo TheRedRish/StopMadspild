0171c61a257c06069c3958a1bdf1694fe5c814ae
Rune <roeddik@live.dk>
2024-11-14 17:54:41 +0100

    Removed test

--- src/test/java/com/example/stopmadspild/StopMadspildApplicationTests.java
+++ src/test/java/com/example/stopmadspild/StopMadspildApplicationTests.java
@@ -3,11 +3,6 @@ package com.example.stopmadspild;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class StopMadspildApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
